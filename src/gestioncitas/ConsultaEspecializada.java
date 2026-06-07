d462cd6bdbc75385080f2313cfe28c7d1280ef88
Alejandro Chavarria <alechavarria2022@gmail.com>
2026-06-07 15:28:21 -0600

    Agrega correccion de tipo de clases

--- src/gestioncitas/ConsultaEspecializada.java
+++ src/gestioncitas/ConsultaEspecializada.java
@@ -10,11 +10,6 @@ package gestioncitas;
  */
 public class ConsultaEspecializada {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
